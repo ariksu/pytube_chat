33aeeee21939130c187dd3a9c72062fe204f7c65
Ariksu <ariksu@gmail.com>
2018-03-10 13:02:28 +0300

    Removed google library, now clean requests, and only oauth2 is remote and httplib2-dependant

--- chat_concept.py
+++ chat_concept.py
@@ -1,47 +1,51 @@
 from oauth2client.tools import argparser
 from oauth2client.file import Storage
 import requests
-from broadcast import get_authenticated_service, list_broadcasts
 import sys
 from pprint import pprint
-
-VALID_BROADCAST_STATUSES = ("all", "active", "completed", "upcoming",)
-CHAT_API_URL = "https://www.googleapis.com/youtube/v3/liveChat/messages"
-
-argparser.add_argument("--broadcast-status", help="Broadcast status",
-                       choices=VALID_BROADCAST_STATUSES, default=VALID_BROADCAST_STATUSES[0])
-args = argparser.parse_args()
-youtube = get_authenticated_service(args)
-
-active_streams = list_broadcasts(youtube, 'active')
-
-print(active_streams)
-stream_id = active_streams[0]['id']
-chat_id = active_streams[0]['snippet']['liveChatId']
-assert (type(stream_id) is str), f"Fail to get stream id, found {stream_id}"
-assert (type(chat_id) is str), f"Fail to get stream id, found {chat_id}"
-
-storage = Storage("%s-oauth2.json" % sys.argv[0])
-credentials = storage.get()
+from oauth_pile import oauth_wrapper
 
 
-params = {
-    'liveChatId': chat_id,
-    'part':'authorDetails,snippet',
-}
+def stream(token):
+    params = {
+        'broadcastStatus': 'active',
+        'part': 'id,snippet',
+    }
+    headers = {
+        'Authorization': f"Bearer {token}",
 
-print(credentials.access_token)
-headers = {
-    'Authorization':f"Bearer {credentials.access_token}",
+    }
+    yt_response = requests.get('https://www.googleapis.com/youtube/v3/liveBroadcasts', params=params,
+                               headers=headers)
+    id = yt_response.json()['items'][0]['id']
+    name = yt_response.json()['items'][0]['snippet']['title']
+    chat_id = yt_response.json()['items'][0]['snippet']['liveChatId']
+    return id, name, chat_id
 
 
-}
+def chat_items(id, token):
+    params = {
+        'liveChatId': chat_id,
+        'part': 'authorDetails,snippet',
+    }
+    headers = {
+        'Authorization': f"Bearer {token}",
 
-chat_response = requests.get('https://www.googleapis.com/youtube/v3/liveChat/messages', params=params, headers=headers)
-chat=chat_response.json()['items']
-cleaned_chat = [{'name':message['authorDetails']['displayName'],
-                'text':message['snippet']['displayMessage'],}
+    }
+    chat_response = requests.get('https://www.googleapis.com/youtube/v3/liveChat/messages', params=params,
+                                 headers=headers)
+    return chat_response.json()['items']
+
+
+# TODO refactor at least oauth call
+storage_name = "%s-oauth2.json" % sys.argv[0]
+credentials = oauth_wrapper(storage_name)
+auth = credentials.access_token
+
+stream, name, chat_id = stream(auth)
+chat = chat_items(chat_id, auth)
+cleaned_chat = [{'name': message['authorDetails']['displayName'],
+                 'text': message['snippet']['displayMessage'], }
                 for message in chat]
-
 
 pprint(cleaned_chat)
